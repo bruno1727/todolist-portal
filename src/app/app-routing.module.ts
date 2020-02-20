dfcc52e3d7458afd4195c09a4662c22a173c4503
Bruno Morais <brunomoraisc@hotmail.com>
2020-02-20 00:09:33 -0300

    criando rota default e incluindo componente principal na app.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TaskManagerComponent } from './task-manager/task-manager/task-manager.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'task-manager', component: TaskManagerComponent },
+  { path: '', redirectTo: '/task-manager', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
